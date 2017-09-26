d25a01eb9975389cb2f26aeb6825151e4beb8750
rsevilla87 <rsevcan@gmail.com>
2017-09-26 17:08:05 +0200

    Modify

--- punsbot.py
+++ punsbot.py
@@ -14,7 +14,7 @@ sys.setdefaultencoding('utf-8')
 
 allowed_chars_puns = string.ascii_letters + " " + string.digits + "áéíóúàèìòùäëïöü"
 allowed_chars_triggers = allowed_chars_puns + "^$.*+?(){}\\[]<>=-"
-version = "0.4.2"
+version = "0.4.3"
 
 if 'TOKEN' not in os.environ:
     print("missing TOKEN.Leaving...")
